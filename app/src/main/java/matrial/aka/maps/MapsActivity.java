8b9e32ec2f6f892d8399c1683b11acad8eeb9405
syntaxSizer <kakuma4never@gmail.com>
2016-06-27 17:41:06 +0300

    get user location and update the viewport

--- app/src/main/java/matrial/aka/maps/MapsActivity.java
+++ app/src/main/java/matrial/aka/maps/MapsActivity.java
@@ -1,6 +1,9 @@
 package matrial.aka.maps;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.view.ViewTreeObserver;
 import android.widget.Button;
@@ -65,6 +68,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     }
                 }
         );
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+        mMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
+            @Override
+            public void onMyLocationChange(Location location) {
+                LatLng ll = new LatLng(location.getLatitude(), location.getLongitude());
+                addPointToViewPort(ll);
+                // we only want to grab the location once, to allow the user to pan and zoom freely.
+                mMap.setOnMyLocationChangeListener(null);
+            }
+        });
+
+
     }
 
     }
