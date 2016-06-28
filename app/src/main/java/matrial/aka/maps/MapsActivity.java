ae0d333767147a60514b0528fd5d2c66247ed83e
syntaxSizer <kakuma4never@gmail.com>
2016-06-28 23:48:28 +0300

    add place picker

--- app/src/main/java/matrial/aka/maps/MapsActivity.java
+++ app/src/main/java/matrial/aka/maps/MapsActivity.java
@@ -1,15 +1,24 @@
 package matrial.aka.maps;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlacePicker;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -20,6 +29,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private static final int REQUEST_PLACE_PICKER = 1;
     private GoogleMap mMap;
     private GoogleApiClient mGoogleApiClient;
 
@@ -46,6 +56,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.animateCamera(CameraUpdateFactory.newLatLngBounds(mBounds.build(),
                 findViewById(R.id.checkout_button).getHeight()));
     }
+
+    /**
+     * Prompt the user to check out of their location. Called when the "Check Out!" button
+     * is clicked.
+     */
+    public void checkOut(View view) {
+        try {
+            PlacePicker.IntentBuilder intentBuilder = new PlacePicker.IntentBuilder();
+            Intent intent = intentBuilder.build(this);
+            startActivityForResult(intent, REQUEST_PLACE_PICKER);
+        } catch (GooglePlayServicesRepairableException e) {
+            GoogleApiAvailability.getInstance().getErrorDialog(this, e.getConnectionStatusCode(),
+                    REQUEST_PLACE_PICKER);
+        } catch (GooglePlayServicesNotAvailableException e) {
+            Toast.makeText(this, "Please install Google Play Services!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+
 
 
     /**
@@ -111,6 +141,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         });
 
 
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_PLACE_PICKER) {
+            if (resultCode == Activity.RESULT_OK) {
+                Place place = PlacePicker.getPlace(data, this);
+            } else if (resultCode == PlacePicker.RESULT_ERROR) {
+                Toast.makeText(this, "Places API failure! Check that the API is enabled for your key",
+                        Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 
     }
